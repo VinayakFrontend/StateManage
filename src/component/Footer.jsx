deddc8aa04528c17a73785d01fb207206328ecea
VinayakFrontend <guptavinayak1614@gmail.com>
2025-04-23 10:44:11 +0530

    Update Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Footer.css'
 
-export default function Footer() {
+export default function Footer(counts) {
 
     return (
         <>
@@ -10,8 +10,9 @@ export default function Footer() {
             <li><img src="https://static.vecteezy.com/system/resources/previews/016/326/810/non_2x/clock-icon-transparent-background-free-png.png"  height="40"  alt="" /></li>
             <li><img src="https://cdn-icons-png.freepik.com/512/3963/3963783.png" alt="" height="40"/></li>
             <li><img src="https://www.freeiconspng.com/thumbs/person-icon/clipart--person-icon--cliparts-15.png" alt=""  height="40"/></li>
-            <li><img src="https://www.freeiconspng.com/uploads/church-bell-icon-5.png" alt="" height="50"/><span className="notify">{counts.counter}</span></li>
+            <li><img src="https://www.freeiconspng.com/uploads/church-bell-icon-5.png" alt="" height="50"/> <span className="notify">{counts.counter}</span></li>
         </ul>
+       
         </div>
         
         </>
