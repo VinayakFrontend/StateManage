a2532ee0736ab91a059670d20c8441c8fb5b6e49
VinayakFrontend <guptavinayak1614@gmail.com>
2025-04-23 10:39:59 +0530

    Update Counters.jsx

--- src/component/Counters.jsx
+++ src/component/Counters.jsx
@@ -34,6 +34,7 @@ export default function Counters() {
                 <button  className="inc" onClick={Increment}>+</button>
                
             </div>
+             <Footer counter={count} />
         </>
     )
 }
